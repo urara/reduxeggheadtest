dd8014584fdf93578db99742ebbd78fef703cfc8
大浦修 <osamu@sg2.local>
2016-05-05 00:25:04 +0900

    整形

--- todo.js
+++ todo.js
@@ -169,41 +169,41 @@ const getVisibleTodos = (
 let nextTodoId = 0;
 const TodoApp = ({
 		todos,
-		visibilityFilter	
+		visibilityFilter
 }) => (
-			<div>
-			  <AddTodo
-				  onAddClick={text =>
-						store.dispatch({
-							type: 'ADD_TODO',
-							id: nextTodoId++,
-							text
-						})
-					}
+		<div>
+			<AddTodo
+				onAddClick={text =>
+					store.dispatch({
+						type: 'ADD_TODO',
+						id: nextTodoId++,
+						text
+					})
+				}
 				/>
-				<TodoList 
-					todos={
-						getVisibleTodos(
-							todos,
-							visibilityFilter
-						)
-					}
-					 onTodoClick={id =>
-						 store.dispatch({
-							 type: 'TOGGLE_TODO',
-							 id
-						 })
-					 } />
-				<Footer
-				  visibilityFilter={visibilityFilter}
-					onFilterClick={filter =>
-					  store.dispatch({
-							type: 'SET_VISIBILITY_FILTER',
-							filter
-						})
-				  }
-					/>
-			</div>
+			<TodoList
+				todos={
+					getVisibleTodos(
+						todos,
+						visibilityFilter
+					)
+				}
+				onTodoClick={id =>
+					store.dispatch({
+						type: 'TOGGLE_TODO',
+						id
+					})
+				} />
+			<Footer
+				visibilityFilter={visibilityFilter}
+				onFilterClick={filter =>
+					store.dispatch({
+						type: 'SET_VISIBILITY_FILTER',
+						filter
+					})
+				}
+			/>
+		</div>
 		);
 
 
