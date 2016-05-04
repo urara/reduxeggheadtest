c60ab22590f428f56bf7c946b074635f8652bb5d
大浦修 <osamu@sg2.local>
2016-05-04 14:11:10 +0900

    Todo,TodoListを外部切り出し

--- todo.js
+++ todo.js
@@ -64,6 +64,39 @@ const FilterLink = ({
 	);
 };
 
+const Todo = ({
+	onClick,
+	completed,
+	text
+}) => (
+	<li
+		onClick = {onClick}
+		style={{
+			textDecoration:
+			completed ?
+				'line-through' :
+				'none'
+		}}
+		>
+		{text}
+	</li>	
+);
+
+const TodoList = ({
+	todos,
+	onTodoClick
+}) => (
+	<ul>
+	  {todos.map(todo => 
+		  <Todo 
+			    key={todo.id}
+					{...todo}
+					onClick={() => onTodoClick(todo.id)}
+			/>
+		)}
+	</ul>
+);
+
 const getVisibleTodos = (
 	todos,
 	filter
@@ -109,26 +142,14 @@ class TodoApp extends Component {
 				>
 				  Add Todo	
 				</button>
-				<ul>
-				  {visibleTodos.map(todo => 
-					  <li key={todo.id}
-  						  onClick = {() => {
-								  store.dispatch({
-									  type: 'TOGGLE_TODO',
-									  id: todo.id
-								  })
-   							}}
-								style={{
-									textDecoration:
-									  todo.completed ?
-										  'line-through' : 
-											'none'
-								}}
-						>
-						  {todo.text}
-						</li>	
-					)}
-				</ul>
+				<TodoList 
+				   todos={visibleTodos}
+					 onTodoClick={id =>
+						 store.dispatch({
+							 type: 'TOGGLE_TODO',
+							 id
+						 })
+					 } />
 				<p>
 				  Show:
 					{' '}
