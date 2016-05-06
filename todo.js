7f6970b5342b7181270fba176c0a7bb1009423ca
大浦修 <osamu@sg2.local>
2016-05-06 22:14:47 +0900

    react-reduxのprovider使用

--- todo.js
+++ todo.js
@@ -248,21 +248,7 @@ const TodoApp = () => (
 		</div>
 );
 
-class Provider extends Component {
-	getChildContext() {
-		return {
-			store: this.props.store
-		};
-	}
-	
-	render() {
-		return this.props.children;
-	}
-}
-
-Provider.childContextTypes = {
-	store: React.PropTypes.object
-};
+const { Provider } = ReactRedux;
 
 const { createStore } = Redux;
 
