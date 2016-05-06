dcf2071c2e8b276e67c5cf87bf15788dd9cecc7f
大浦修 <osamu@sg2.local>
2016-05-06 23:02:40 +0900

    mapStateToPropsとmapDispatchToPropsを適用

--- todo.js
+++ todo.js
@@ -200,44 +200,31 @@ const getVisibleTodos = (
 	}
 }
 
-class VisibleTodoList extends Component {
-	componentDidMount() {
-		const { store } = this.context;
-		this.unsubscribe = store.subscribe(() => 
-			this.forceUpdate()
-		);
-	}
-	
-	componentWillUnmount() {
-		this.unsubscribe();
-	}
-	
-	render() {
-		const props = this.props;
-		const { store } = this.context;
-		const state = store.getState();
-		
-		return (
-			<TodoList
-			  todos={
-					getVisibleTodos(
-						state.todos,
-						state.visibilityFilter
-					)
-				}
-				onTodoClick={id =>
-				  store.dispatch({
-						type: 'TOGGLE_TODO',
-						id
-					})
-			  }
-			/>
-		);
-	}
-}
-VisibleTodoList.contextTypes = {
-	store: React.PropTypes.object
-}
+const mapStateToProps = (state) => {
+	return {
+		todos: getVisibleTodos(
+			state.todos,
+			state.visibilityFilter
+		)
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onTodoClick: (id) => {
+			dispatch({
+				type: 'TOGGLE_TODO',
+				id
+			})
+		}
+	};
+};
+
+const { connect } = ReactRedux;
+const VisibleTodoList = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(TodoList);
 
 let nextTodoId = 0;
 const TodoApp = () => (
