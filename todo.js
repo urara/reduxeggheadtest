d38d48e19bac7aecb5c0f4d197e218783098d3c5
大浦修 <osamu@sg2.local>
2016-05-07 11:19:52 +0900

    FilterLinkもconnectで書き換え

--- todo.js
+++ todo.js
@@ -60,44 +60,34 @@ const Link = ({
 	);
 };
 
-class FilterLink extends Component {
-	componentDidMount() {
-		const { store } = this.context;
-		this.unsubscribe = store.subscribe(() => 
-			this.forceUpdate()
-		);
+const mapStateToLinkProps = (
+	state,
+	ownProps
+) => {
+	return {
+		active:
+			ownProps.filter === 
+			state.visibilityFilter
 	}
-	
-	componentWillUnmount() {
-		this.unsubscribe();
-	}
-	
-	render() {
-  	const props = this.props;
-		const {store} = this.context;
-	  const state = store.getState();
-		
-		return (
-			<Link
-			  active={
-					props.filter === state.visibilityFilter
-				}
-				onClick={() => {
-					store.dispatch({
+};
+
+const mapDispatchToLinkProps = (
+	dispatch,
+	ownProps	
+) => {
+	return {
+		onClick: () => {dispatch({
 						type: 'SET_VISIBILITY_FILTER',
-						filter: props.filter
-					})
-				}}
-			>
-				{props.children}
-			</Link>
-		);
-	}	
+						filter: ownProps.filter
+		  });
+		}
+	};
 }
 
-FilterLink.contextTypes = {
-	store: React.PropTypes.object
-};
+const FilterLink = connect(
+	mapStateToLinkProps,
+	mapDispatchToLinkProps
+)(Link);
 
 const Footer = () => (
 	<p>
